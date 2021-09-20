c14c3774920a1f851e6d228b6abbabb61e58cbc3
차은채 <jjcec@naver.com>
2021-09-20 17:07:18 +0900

    Apply Material UI

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
-import Customer from './components/Customer'
+import React, { Component } from 'react'; // function일 때는 없었지만 Class를 쓴다면 추가
+import Customer from './components/Customer';
 import './App.css';
+import Paper from '@material-ui/core/Paper'; // 컴포넌트의 외부를 감싸기 위해 사용하는 컴포넌트 중 하나
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = theme => ({
+  root: {
+    width: "100%",
+    marginTop: theme.spacing.unit*3, // 여백 3
+    overflowX: "auto"
+  },
+  table: {
+    minWidth: 1080
+  }
+})
 
 const customers = [
   {
@@ -29,26 +48,43 @@ const customers = [
   
 ]
 
-function App() {
-  return (
-    <div>
-      {
-        customers.map(c => { // c로 순회
-          return (
-            <Customer
-              key={c.id} // map을 사용할 때 key값을 넣어줘야한다.
-              id={c.id}
-              image={c.image}
-              name={c.name}
-              birthday={c.birthday}
-              gender={c.gender}
-              job={c.job}
-            />
-          )
-        })
-      }
-    </div>
-  );
+class App extends Component { // theme 적용하기 위해 class로 바꿈
+  render() { // Class로 바꾸면 render함수로 감싸야한다.
+    const { classes } = this.props;
+    return (
+      <Paper className={classes.root}>
+        <Table className={classes.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell>번호</TableCell>
+              <TableCell>이미지</TableCell>
+              <TableCell>이름</TableCell>
+              <TableCell>생년월일</TableCell>
+              <TableCell>성별</TableCell>
+              <TableCell>직업</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {
+              customers.map(c => { // c로 순회, 한줄로도 작성 가능
+                return (
+                  <Customer
+                    key={c.id} // map을 사용할 때 key값을 넣어줘야한다.
+                    id={c.id}
+                    image={c.image}
+                    name={c.name}
+                    birthday={c.birthday}
+                    gender={c.gender}
+                    job={c.job}
+                  />
+                )
+              })
+            }
+          </TableBody>
+        </Table>
+      </Paper>
+    );
+  }
 }
 
-export default App;
+export default withStyles(styles)(App);
