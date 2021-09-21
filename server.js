dcb9b976f4fe58e2bbbc538588bee74e3b91e37a
차은채 <jjcec@naver.com>
2021-09-21 20:36:35 +0900

    Create Customer Database Table & Node.js Database Connection Settings

--- server.js
+++ server.js
@@ -1,10 +1,35 @@
+// const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
+// const { connect } = require('http2');
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// const data = fs.readFileSync('./database.json');
+// const conf = JSON.parse(data); // 환경설정 데이터 파싱해서 가져오기
+// const mysql = require('mysql'); // mysql 라이브러리 불러와서 mysql변수에 담을 수 있도록
+
+// const connection = mysql.createConnection({
+//     host: conf.host,
+//     user: conf.user,
+//     password: conf.password,
+//     port: conf.port,
+//     database: conf.database
+// });
+
+// connection.connect();
+
+// app.get('/api/customers', (req, res) => {
+//     connection.query(
+//         "SELECT * FROM customer",
+//         (err, rows, fields) => {
+//             res.send(rows);
+//         }
+//     )
+// })
 
 app.get('/api/customers', (req, res) => {
     res.send([
